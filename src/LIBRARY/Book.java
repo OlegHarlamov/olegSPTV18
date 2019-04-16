77f852acba75e3f801644b5745a5a50cc38606f0
user <user@student303-3>
2019-04-16 13:35:36 +0300

    Hello

--- src/LIBRARY/Book.java
+++ src/LIBRARY/Book.java
@@ -1,35 +1,10 @@
 package LIBRARY;
 public class Book{
-    String title;
-    public Book(String title){
-this.title = title;
-}
-    public static void main(String[] args){
-        Author[] authorArray = {new Author(), 
-            new Author(), 
-            new Author() 
-    };
-        System.out.println(authorArray[1].getAuthors()[0]);
-        System.out.println(authorArray[1].getAuthors()[1]);
-        System.out.println(authorArray[1].getAuthors()[2]);
-        System.out.println(authorArray[1].getAuthors()[3]);
-        Book book;
-        book = new Book("Нетерпение сердца");
-         String [] books = new String[5];
-        books[0] = "Title book: " + book.title + " author: " + authorArray[0].getAuthors()[0];
-       book.title = "Красное и черное";
-        books[1] = "Title book: " + book.title + " author: " + authorArray[0].getAuthors()[1];
-        book.title = "Пармская обитель";
-        books[2] = "Title book: " + book.title + " author: " + authorArray[1].getAuthors()[1];
-        book.title = "Последнее волшебство";
-        books[3] = "Title book: " + book.title + " author: " + authorArray[1].getAuthors()[2];
-        book.title = "Собор Парижской Богоматери";
-        books[4] = "Title book: " + book.title + " authors: " + authorArray[1].getAuthors()[3] + ", " +authorArray[1].getAuthors()[3];
-        for(String x: books){
-            System.out.println(x);
-        }
-}
-    public String toString(){
-        return title;
+    private String[] books = {"Дорогие гости","Последние Девушки","Война и мир","И все мы будем счастливы","Не самое главное","Молитва морю","Рай где-то рядом","Улыбнись страху. Пробуждение истинного сердца отваги","Сантехник. Твое мое колено","Чапаев и Пустота"};
+    public String[] getBooks() {
+        return books;
+    }
+    public void setBooks(String title) {
+        this.books = books;
     }
 }
---
+++ src/LIBRARY/Lib.java
@@ -0,0 +1,37 @@
+package LIBRARY;
+public class Lib{
+    String title;
+    public Lib(String title){
+this.title = title;
+}
+    public static void main(String[] args){
+        Author[] authorArray = {new Author(), 
+            new Author(), 
+                 new Author()                       
+    };
+        Book[] bookArray = {new Book(),
+        new Book(),
+        new Book()
+        };
+         for (int i = 0; i < 11; i++) {
+         System.out.println(authorArray[0].getAuthors()[11]);
+         }
+         String [] books = new String[10];
+        books[0] = "Title book: " + bookArray[0].getBooks()[0] + " author: " + authorArray[0].getAuthors()[0];
+        books[1] = "Title book: " + bookArray[0].getBooks()[1] + " author: " + authorArray[0].getAuthors()[1];
+        books[2] = "Title book: " + bookArray[0].getBooks()[2] + " author: " + authorArray[1].getAuthors()[2];
+        books[3] = "Title book: " + bookArray[0].getBooks()[3] + " author: " + authorArray[1].getAuthors()[3];
+        books[4] = "Title book: " + bookArray[0].getBooks()[4] + " authors: " + authorArray[1].getAuthors()[4] +", " + authorArray[1].getAuthors()[5];
+        books[5] = "Title book: " + bookArray[0].getBooks()[5] + " author: " + authorArray[1].getAuthors()[5];
+        books[6] = "Title book: " + bookArray[0].getBooks()[6] + " authors: " + authorArray[1].getAuthors()[6]+ ", "+authorArray[1].getAuthors()[3];
+        books[7] = "Title book: " + bookArray[0].getBooks()[7] + " authors: " + authorArray[1].getAuthors()[7]+ ", "+authorArray[1].getAuthors()[10];
+        books[8] = "Title book: " + bookArray[0].getBooks()[8] + " authors: " + authorArray[1].getAuthors()[8]+ ", "+authorArray[1].getAuthors()[9];
+        books[9] = "Title book: " + bookArray[0].getBooks()[9] + " authors: " + authorArray[1].getAuthors()[9]+ ", "+authorArray[1].getAuthors()[11];
+        for(String x: books){
+            System.out.println(x);
+        }
+}
+    public String toString(){
+        return title;
+    }
+}
